bb4f109d9b206c2a5b48fcbfc16335f6d313c90e
trandat9029 <tranledatvp@gmail.com>
2025-07-03 13:53:34 +0700

    Orangy-Hero-banner: style hero-banner layout responsive

---
+++ src/assets/js/orangy.js
@@ -0,0 +1,12 @@
+/* update image */ 
+
+function updateHeroImg(){
+    const img = document.querySelector('.hero-banner__background-img');
+
+    if(!img) return;
+
+    const isMobile = window.innerWidth <= 768;
+    img.src =isMobile ? "/src/assets/img/hero-banner-mobile.png" : "/src/assets/img/hero-banner.png"
+}
+window.addEventListener('load', updateHeroImg);
+window.addEventListener('resize', updateHeroImg);
